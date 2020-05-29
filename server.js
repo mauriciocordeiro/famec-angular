e05ed3768eb8594c7cdf89c63ffd1d8fd7623319
Maurício Cordeiro <mauriciocordeiro@live.com>
2020-05-29 11:26:38 -0300

    deploy on heroku, attempt #1

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/famec-angular'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/famec-angular/'}),
+);
+
+app.listen(process.env.PORT || 8080);
